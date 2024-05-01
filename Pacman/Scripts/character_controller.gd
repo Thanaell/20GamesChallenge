d2eca6ff73a8d6acf2ea560a9b9479c2d819a139
Thanaell <gaelle_clavelin@orange.fr>
2024-05-02 00:16:38 +0200

    Add sprites, tilemap and pacman movement

---
+++ Pacman/Scripts/character_controller.gd
@@ -0,0 +1,62 @@
+extends Area2D
+
+const max_speed: float = 100.0
+const arrive_distance: float = 0.1
+
+var has_speed: bool = false
+var is_arrived: bool = true
+var should_change_direction: bool = false
+var direction: Vector2 = Vector2(0, 0)
+var direction_buffer: Array = []
+var path: PackedVector2Array = PackedVector2Array()
+
+@onready var tile_map: Pathfinder = $"../level"
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_right") && direction != Vector2.RIGHT:
+		direction_buffer.clear()
+		direction_buffer.append(Vector2.RIGHT)
+	if Input.is_action_just_pressed("ui_left") && direction != Vector2.LEFT:
+		direction_buffer.clear()
+		direction_buffer.append(Vector2.LEFT)
+	if Input.is_action_just_pressed("ui_up") && direction != Vector2.UP:
+		direction_buffer.clear()
+		direction_buffer.append(Vector2.UP)
+	if Input.is_action_just_pressed("ui_down") && direction != Vector2.DOWN:
+		direction_buffer.clear()
+		direction_buffer.append(Vector2.DOWN)
+
+	if !has_speed:
+		if len(direction_buffer) > 0:
+			if tile_map.is_tile_traversable(position, direction_buffer[0]):
+				direction = direction_buffer.pop_front()
+				has_speed = true
+				sprite.play("default")
+				update_path()
+			else:
+				direction_buffer.clear()
+	else:
+		if len(direction_buffer) > 0:
+			if tile_map.is_tile_traversable(position, direction_buffer[0]):
+				should_change_direction = true
+
+		position += delta * max_speed * direction
+		is_arrived = (path[0] - position).dot(direction) < 0
+		if is_arrived:
+			if should_change_direction:
+				direction = direction_buffer.pop_front()
+				should_change_direction = false
+			if tile_map.is_tile_traversable(position, direction):
+				update_path()
+			else:
+				position = path[0]
+				has_speed = false
+				sprite.pause()
+
+
+func update_path():
+	path.clear()
+	path = tile_map.find_path_with_direction(position, direction)
+	path.remove_at(0)
---
+++ Pacman/Scripts/pathfinder.gd
@@ -0,0 +1,40 @@
+class_name Pathfinder extends TileMap
+
+const tile_size: int = 16
+const tile_map_x: int = 19
+const tile_map_y: int = 18
+
+var a_star = AStarGrid2D.new()
+
+
+func _ready():
+	a_star.region = Rect2i(0, 0, tile_map_x, tile_map_y)
+	a_star.cell_size = Vector2(tile_size, tile_size)
+	a_star.offset = 0.5 * Vector2(tile_size, tile_size)
+	a_star.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	a_star.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	a_star.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	a_star.update()
+
+	for i in range(a_star.region.position.x, a_star.region.end.x):
+		for j in range(a_star.region.position.y, a_star.region.end.y):
+			var pos = Vector2i(i, j)
+			var tile_data: TileData = get_cell_tile_data(0, pos)
+			if !tile_data.get_custom_data("is_traversable"):
+				a_star.set_point_solid(pos)
+
+
+func is_tile_traversable(source_coord: Vector2, direction: Vector2) -> bool:
+	var target_map_index: Vector2i = local_to_map(source_coord + (tile_size * direction))
+	var tile_data: TileData = get_cell_tile_data(0, target_map_index)
+	return tile_data.get_custom_data("is_traversable")
+
+
+func find_path(source_coord: Vector2, target_coord: Vector2) -> PackedVector2Array:
+	var source_point: Vector2i = local_to_map(source_coord)
+	var target_point: Vector2i = local_to_map(target_coord)
+	return a_star.get_point_path(source_point, target_point)
+
+
+func find_path_with_direction(source_coord: Vector2, direction: Vector2) -> PackedVector2Array:
+	return find_path(source_coord, source_coord + (tile_size * direction))
